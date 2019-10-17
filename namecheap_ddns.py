9132b48b51728fd1f7b775d58d980dec91d32c2f
Price Comstock <price.comstock@gmail.com>
2019-10-16 22:09:49 -0400

    refactored api calls

--- namecheap_ddns.py
+++ namecheap_ddns.py
@@ -4,12 +4,14 @@ from config import domains
 
 my_public_ip = requests.get('https://api.ipify.org?format=json').json()['ip']
 
-TEMPLATE_URL = 'https://dynamicdns.park-your-domain.com/update?host=[host]&domain=[domain_name]&password=[ddns_password]&ip=[your_ip]'
+template_url = 'https://dynamicdns.park-your-domain.com/update?host={host}&domain={domain}&password={ddns_key}&ip={ip}'
 
 for domain in domains:
+    key = domains[domain]['key']
     for host in domains[domain]['hosts']:
-        url = TEMPLATE_URL.replace('[host]', host).replace('[domain_name]', domain).replace(
-            '[ddns_password]', domains[domain]['key']).replace('[your_ip]', my_public_ip)
+
+        url = template_url.format(
+            host=host, domain=domain, ddns_key=key, ip=my_public_ip)
         print('Calling ' + url)
         r = requests.get(url)
         print(str(r))
